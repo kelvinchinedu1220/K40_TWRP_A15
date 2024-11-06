e2d51b0e025def346c48cffafc5ce4c3eef0a5bd
Kevin chinedu <kelvinchinedu97@gmail.com>
2024-11-06 17:42:32 +0100

    Update and rename twrp_alioth.mk to omni_alioth.mk

--- omni_alioth.mk
+++ omni_alioth.mk
@@ -1,7 +1,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-# Copyright (C) 2021-2024 The OrangeFox Recovery Project
+# Copyright (C) 2021-2024 The pbrp Recovery Project
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
@@ -13,14 +13,14 @@ DEVICE_PATH := device/xiaomi/$(PRODUCT_RELEASE_NAME)
 # Inherit from alioth device
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
 
-# Inherit any OrangeFox-specific settings
+# Inherit any pbrp-specific settings
 $(call inherit-product-if-exists, $(DEVICE_PATH)/fox_mikona.mk)
 
-# Inherit some common twrp stuff
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Inherit some common pbrp stuff
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := twrp_$(PRODUCT_RELEASE_NAME)
+PRODUCT_NAME := omni_$(PRODUCT_RELEASE_NAME)
 PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
 PRODUCT_BRAND := POCO
 PRODUCT_MANUFACTURER := Xiaomi
