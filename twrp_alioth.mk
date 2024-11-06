e2da2df73f4b25a674c8cb8efa13b430b286a589
Kevin chinedu <kelvinchinedu97@gmail.com>
2024-11-06 19:11:42 +0100

    Update twrp_alioth.mk

--- twrp_alioth.mk
+++ twrp_alioth.mk
@@ -7,10 +7,15 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Inherit some common pb stuff.
-$(call inherit-product, vendor/pb/config/common.mk)
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit any OrangeFox-specific settings
 $(call inherit-product-if-exists, $(DEVICE_PATH)/fox_mikona.mk)
@@ -19,7 +24,7 @@ $(call inherit-product-if-exists, $(DEVICE_PATH)/fox_mikona.mk)
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
 PRODUCT_DEVICE := alioth
-PRODUCT_NAME := omni_alioth
+PRODUCT_NAME := twrp_alioth
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := M2012K11AC
 PRODUCT_MANUFACTURER := xiaomi
