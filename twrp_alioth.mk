f52970be111ce838a3dfcd6ce5f806f8d193348c
Kevin chinedu <kelvinchinedu97@gmail.com>
2024-11-06 19:06:51 +0100

    Update twrp_alioth.mk

--- twrp_alioth.mk
+++ twrp_alioth.mk
@@ -1,30 +1,27 @@
 #
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
 # SPDX-License-Identifier: Apache-2.0
 #
-# Copyright (C) 2021-2024 The OrangeFox Recovery Project
-# SPDX-License-Identifier: GPL-3.0-or-later
-#
 
-# Release name
-PRODUCT_RELEASE_NAME := alioth
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-DEVICE_PATH := device/xiaomi/alioth
-
-# Inherit from alioth device
-$(call inherit-product, device/xiaomi/alioth/device.mk)
+# Inherit some common pb stuff.
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit any OrangeFox-specific settings
 $(call inherit-product-if-exists, $(DEVICE_PATH)/fox_mikona.mk)
 
-# Inherit some common twrp stuff
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Inherit from alioth device
+$(call inherit-product, device/xiaomi/alioth/device.mk)
 
-# Device identifier. This must come after all inclusions
-PRODUCT_NAME := twrp_alioth
 PRODUCT_DEVICE := alioth
-PRODUCT_BRAND := POCO
-PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_MODEL := POCO F3
+PRODUCT_NAME := omni_alioth
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := M2012K11AC
+PRODUCT_MANUFACTURER := xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-#
