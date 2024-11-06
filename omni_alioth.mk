b73b04d4d354d5173b6d71eef6f888c878ec9a5e
Kevin chinedu <kelvinchinedu97@gmail.com>
2024-11-06 19:04:13 +0100

    Update omni_alioth.mk

--- omni_alioth.mk
+++ omni_alioth.mk
@@ -1,27 +1,24 @@
 #
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
 # SPDX-License-Identifier: Apache-2.0
 #
-# Copyright (C) 2021-2024 The pbrp Recovery Project
-# SPDX-License-Identifier: GPL-3.0-or-later
-#
 
-DEVICE_PATH := device/xiaomi/alioth
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Release name
-PRODUCT_RELEASE_NAME := alioth
+# Inherit some common pb stuff.
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-# Inherit some common pbrp stuff
-$(call inherit-product, vendor/pb/config/common.mk)
-
-# Device identifier. This must come after all inclusions
-PRODUCT_NAME := omni_alioth
 PRODUCT_DEVICE := alioth
-PRODUCT_BRAND := POCO
-PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_MODEL := POCO F3
+PRODUCT_NAME := omni_alioth
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := M2012K11AC
+PRODUCT_MANUFACTURER := xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-#
