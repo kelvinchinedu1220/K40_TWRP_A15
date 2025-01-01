b723bcef985a05a835be99e41a9a4e19beb9e6d5
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-01-01 10:54:18 +0100

    Update twrp_alioth.mk

--- twrp_alioth.mk
+++ twrp_alioth.mk
@@ -1,27 +1,28 @@
 #
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
 # SPDX-License-Identifier: Apache-2.0
 #
-# Copyright (C) 2021-2024 The OrangeFox Recovery Project
-# SPDX-License-Identifier: GPL-3.0-or-later
-#
 
-# Release name
-PRODUCT_RELEASE_NAME := alioth
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-DEVICE_PATH := device/xiaomi/alioth
+# Inherit from this product for devices that support only 64-bit apps using:
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+
+# Enable virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Inherit from alioth device
-$(call inherit-product, $(DEVICE_PATH)/device.mk)
+$(call inherit-product, device/xiaomi/alioth/device.mk)
 
-# Inherit some common twrp stuff
+# Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := munch
 PRODUCT_NAME := twrp_alioth
-PRODUCT_DEVICE := alioth
 PRODUCT_BRAND := POCO
-PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := POCO F3
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
+PRODUCT_MANUFACTURER := xiaomi
